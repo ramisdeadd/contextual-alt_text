81c1ff671e583d156b323690928a1094351acd65
josh <josh>
2024-08-31 19:51:37 +0800

    adds tokenization and second iteration of user authentication

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import os
+from datetime import datetime, timedelta, timezone
 from typing import Union, Annotated
+
+import jwt
 from pydantic import BaseModel
-from fastapi import FastAPI, Request, Depends
+from fastapi import FastAPI, Request, Depends, UploadFile, File, Form, HTTPException, status
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from fastapi import UploadFile, File, Form, HTTPException, status
+from jwt.exceptions import InvalidTokenError
+from passlib.context import CryptContext
 from generate import create_alttext
 
-app = FastAPI()
+SECRET_KEY = "261bf58e0aac77becaad284225519d3d131a74fca519b6e6e39b26f9461edaaf"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-fake_users_db = {
+db = {
     "johnsmith": {
         "username": "johnsmith",
         "full_name": "John Smith",
@@ -28,10 +29,17 @@ fake_users_db = {
         "username": "ryanbang",
         "full_name": "Ryan Bang",
         "email": "ryanbang@gmail.com",
-        "hashed_password": "fakehashedsecret2",
+        "hashed_password": "f3d600bd00b0ce715b50e189dd34d7f12c0bb7173ae49c380cdecae5c74bb279",
         "disabled": True,
     }
 }
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(Token):
+    username: str | None = None
 
 class User(BaseModel):
     username: str
@@ -43,50 +51,98 @@ class UserInDB(User):
     hashed_password: str
 
 
-def fake_hash_password(password: str):
-    return "fakehashed" + password
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
 
 def get_user(db, username: str):
     if username in db:
         user_dict = db[username]
         return UserInDB(**user_dict)
 
-def fake_decode_token(token):
-    user = get_user(fake_users_db, token)
+def authenticate_user(db, username: str, password: str):
+    user = get_user(db, username)
+    if not User:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
     return user
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    user = fake_decode_token(token)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid Authentication Credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid Credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try: 
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
     return user
 
-async def get_current_active_user(current_user: Annotated[User, Depends(get_current_user)]):
+async def get_current_active_user(
+        current_user: Annotated[User, Depends(get_current_user)]
+):
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive User")
     return current_user
 
-@app.get("/users/me")
+@app.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    # login_for_access_token -> authenticate_user -> get_user -> verify_password -> END
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect Username or Password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    # create_access_token -> END
+    access_token = create_access_token(
+        data = {"sub": user.username}, 
+        expires_delta = access_token_expires
+    )
+    return Token(access_token, token_type="bearer")
+
+@app.get("/users/me", response_model = User)
 async def read_users_me(
-    current_user: Annotated[User, Depends(get_current_active_user)]
+    current_user: Annotated[User, Depends(get_current_active_user)],
 ):
     return current_user
 
-@app.post("/token")
-async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    user_dict = fake_users_db.get(form_data.username)
-    if not user_dict:
-        raise HTTPException(status_code=400, detail="Incorrect Username or Password")
-    user = UserInDB(**user_dict)
-    hashed_password = fake_hash_password(form_data.password)
-    if not hashed_password == user.hashed_password:
-        raise HTTPException(status_code=400, detail="Incorrect Username or Password")
-
-    return {"access_token": user.username, "token_type": "bearer"}
+#app.get()
 
 ########
 
