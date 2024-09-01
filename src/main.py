c84d5fcc35cfee098f9e25403dacf8d097226a4f
josh <josh>
2024-09-01 12:52:32 +0800

    changes file directories to include database

---
+++ src/main.py
@@ -0,0 +1,181 @@
+import os
+from datetime import datetime, timedelta, timezone
+from typing import Union, Annotated
+
+import jwt
+from pydantic import BaseModel
+from sqlmodel import Field, SQLModel, create_engine
+from sqlalchemy import Column, Integer
+from fastapi import FastAPI, Request, Depends, UploadFile, File, Form, HTTPException, status
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jwt.exceptions import InvalidTokenError
+from passlib.context import CryptContext
+from generate import create_alttext
+
+SECRET_KEY = "aafb48d530ee71c753e64e6830439b026c9405685c19b8829b8065c881ad2876"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    },
+    "ryanbang": {
+        "username": "ryanbang",
+        "full_name": "Ryan Bang",
+        "email": "ryanbang@gmail.com",
+        "hashed_password": "f3d600bd00b0ce715b50e189dd34d7f12c0bb7173ae49c380cdecae5c74bb279",
+        "disabled": True,
+    }
+}
+
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    username: str
+    email: str = Field(unique=True)
+    password: str
+    disabled: bool
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+class UserInDB(User):
+    hashed_password: str
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+sqlite_file_name = "alt-text.db"
+sqlite_url = f"sqlite:///../data/{sqlite_file_name}"
+
+engine = create_engine(sqlite_url, echo=True)
+SQLModel.metadata.create_all(engine)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+def authenticate_user(db, username: str, password: str):
+    user = get_user(db, username)
+    if not User:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid Credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try: 
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+async def get_current_active_user(
+        current_user: Annotated[User, Depends(get_current_user)]
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive User")
+    return current_user
+
+@app.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+) -> Token:
+    # login_for_access_token -> authenticate_user -> get_user -> verify_password -> END
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect Username or Password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    # create_access_token -> END
+    access_token = create_access_token(
+        data = {"sub": user.username}, 
+        expires_delta = access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
+# read_users_me -> get_current_active_user -> get_current_user -> get_user
+@app.get("/users/me", response_model = User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return current_user
+
+#app.get()
+
+########
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("/pages/index.html", {"request": request})
+
+@app.get("/login", response_class=HTMLResponse)
+async def login(request: Request):
+    return templates.TemplateResponse("/pages/login.html", {"request": request})
+
+@app.post("/")
+async def alt_text(text: Annotated[str, Form()], img: UploadFile = File(...)):
+    img_content = await img.read()
+    img_path = f"images/{img.filename}"
+    with open(img_path, "wb") as f:
+        f.write(img_content)
+
+    alt_text = create_alttext(text, img_path)
+
+    return JSONResponse(content={
+        "Generated Alt-Text": alt_text, 
+    }) 
