f3d97e4a367034578a47e525039d7a2316f78d2f
ramcodes <roymarven9@gmail.com>
2024-09-09 19:21:24 +0800

    password confirmation implemented

--- src/static/signup.js
+++ src/static/signup.js
@@ -1,3 +1,20 @@
+var password = document.getElementById("password_input")
+  , confirm_password = document.getElementById("conf_password");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+
+
+
+
 document.getElementById("signup-form").addEventListener("submit", async (e) => {
     e.preventDefault();
     const form = document.getElementById("signup-form")
