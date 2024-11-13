ac5569c77861753df21ffa697115d7bfbff27da2
josh <anthony21josh@gmail.com>
2024-11-13 14:04:01 +0800

    adds functionality for copy alt-text and image caption to clipboard

--- src/static/scripts.js
+++ src/static/scripts.js
@@ -1,3 +1,6 @@
+alt_text_output = document.getElementById("getalttext")
+image_caption_output = document.getElementById("getcaption")
+
 document.getElementById("upload-article").addEventListener("submit", async (e) => {
     e.preventDefault();
     const form = document.getElementById("upload-article")
@@ -16,13 +19,13 @@ document.getElementById("upload-article").addEventListener("submit", async (e) =
 
     const result = await response.json();
     
-    document.getElementById("getalttext").value = result['generated-alt-text'];
-    document.getElementById("getcaption").value = result['generated-image-caption'];
+    alt_text_output.value = result['generated-alt-text'];
+    image_caption_output.getElementById("getcaption").value = result['generated-image-caption'];
 });
 
 
-var loadFile = function(event) {
-    var output = document.getElementById('output');
+let loadFile = function(event) {
+    let output = document.getElementById('output');
     const border = document.querySelector('.uploadimage');
     const border2 = document.getElementById('imagebox')
     output.src = URL.createObjectURL(event.target.files[0]);
@@ -32,3 +35,21 @@ var loadFile = function(event) {
 
     border.style.border = "unset";
 };  
+
+document.getElementById("copy-alt").addEventListener("click", async (e) => {
+    e.preventDefault()
+    alt_text_output.select();
+    alt_text_output.setSelectionRange(0, 9999);
+
+    navigator.clipboard.writeText(alt_text_output.value);
+    console.log("Alt-Text Copied to Clipboard")
+});
+
+document.getElementById("copy-caption").addEventListener("click", async (e) => {
+    e.preventDefault()
+    image_caption_output.select();
+    image_caption_output.setSelectionRange(0, 9999);
+
+    navigator.clipboard.writeText(image_caption_output.value);
+    console.log("Image Caption Copied to Clipboard")
+});
